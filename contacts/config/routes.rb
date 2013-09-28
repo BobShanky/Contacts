3a9e99ddba63295c08ef64992a72c022617a9598
Brian Smith <bjsmithku90@gmail.com>
2013-09-28 14:50:05 -0500

    app

--- contacts/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Contacts::Application.routes.draw do
-  resources :contacts
-
-  resources :companies
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  root to: "welcome#index"
-end
